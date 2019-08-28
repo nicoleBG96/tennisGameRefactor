3e99cf557ad72e7ccb31a19a624b89f63a818945
nicole <nikybg96@gmail.com>
2019-08-28 00:16:16 -0400

    volver global score

--- src/TennisGame2.java
+++ src/TennisGame2.java
@@ -7,6 +7,8 @@ public class TennisGame2 implements TennisGame
     
     private String player1Name;
     private String player2Name;
+    
+    String score = "";
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -14,50 +16,44 @@ public class TennisGame2 implements TennisGame
     }
 
     public String getScore(){
-    	String score = "";
-        score = tie(score);
-        score = deuce(score);
-        score = normal(score);
-        score = advantage(score);
-        score = win(score);
+        tie();
+        deuce();
+        normal();
+        advantage();
+        win();
         return score;
     }
 
-	private String normal(String score) {
+	private void normal() {
 		if (P1Points !=P2Points  && 
 		(P1Points<4 || P2Points<4))
 			score = getLiteralScore(P1Points) 
 			+ "-" 
 			+ getLiteralScore(P2Points);
-		return score;
 	}
 
-	private String deuce(String score) {
+	private void deuce() {
 		if (P1Points==P2Points && P1Points>=3)
 			score = "deuce";
-		return score;
 	}
 
-	private String tie(String score) {
+	private void tie() {
 	 if ( P1Points == P2Points && P1Points < 4)
 		 score = getLiteralScore(P1Points)+ "-All";
-	return score;
 	}
 
-	private String win(String score) {
+	private void win() {
 		if (P1Points>=4 && P2Points>=0 && (P1Points-P2Points)>=2)
             score = "Win for player1";
         if (P2Points>=4 && P1Points>=0 && (P2Points-P1Points)>=2)
             score = "Win for player2";
-        return score;
 	}
 
-	private String advantage(String score) {
+	private void advantage() {
 		if (P1Points > P2Points && P2Points >= 3)
             score = "Advantage player1";
         if (P2Points > P1Points && P1Points >= 3)
             score = "Advantage player2";
-        return score;
 	}
 
 	private String getLiteralScore(int playerPoints) {
